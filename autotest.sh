e2f3e5eca139f6c411c806579d2df76c891fbad5
imcauley <mcauleyisaac@gmail.com>
2020-03-24 12:30:31 -0600

    added better automated testing

--- autotest.sh
+++ autotest.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 for f in ./testfiles/*
 do
-    echo "Testing file: $f"
-    cat $f
-    echo " "
-    echo "Compiler Output:"
-    ./compiler $f
-    echo "================================="
+    echo ${f:12:256}
+    PF=${f:12:1}
+    PASS=p
+    FAIL=f
 
+    if [ $PF = $PASS ]
+    then
+        ./compiler $f > /dev/null
+        if [ $? -eq 255 ]
+        then
+            echo -e "${GREEN}PASS PASS${NC}"
+        else
+            echo -e "${RED}PASS FAIL${NC}"
+        fi
+    fi
+
+    if [ $PF = $FAIL ]
+    then
+        ./compiler $f > /dev/null
+        if [ $? -eq 255 ]
+        then
+            echo -e "${GREEN}FAIL PASS${NC}"
+        else
+            echo -e "${RED}FAIL FAIL${NC}"
+        fi
+    fi
 done
